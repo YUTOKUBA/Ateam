5acbf014d55bd8e9da602d7f4a2fefa8bb83de23
KBC12006118\kbc12006118 <koudai-arashiro-20h@stu.kbc.ac.jp>
2021-10-12 09:39:24 +0900

    コントローラーで動かせる土台

--- New Unity Project/Assets/Script/Tilt.cs
+++ New Unity Project/Assets/Script/Tilt.cs
@@ -7,6 +7,7 @@ public class Tilt : MonoBehaviour
     Transform target;
     float speed = 90f;
     float back_speed = 60f;
+    bool keyon = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,59 +20,40 @@ public class Tilt : MonoBehaviour
         float step = speed * Time.deltaTime;
         float back_step = back_speed * Time.deltaTime;
 
-
-        //if (Input.GetAxisRaw("Vertical") == 1)
-        //{
-        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(30f, 0, 0), step);
-        //}
-        //if (Input.GetAxisRaw("Vertical") == -1)
-        //{
-        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(-30f, 0, 0), step);
-        //}
-        //else if (Input.GetAxisRaw("Vertical") == 0)
-        //{
-        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, 0), back_step);
-        //}
-
-        //if (Input.GetAxisRaw("Horizontal") == 1)
-        //{
-        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, 30f), step);
-        //}
-        //if (Input.GetAxisRaw("Horizontal") == -1)
-        //{
-        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, -30f), step);
-        //}
-        //else if (Input.GetAxisRaw("Horizontal") == 0)
-        //{
-        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, 0), back_step);
-        //}
-
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetAxisRaw("Vertical") == 1)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(30f, 0, 0), step);
         }
+        else
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(-30f, 0, 0), back_step);
+        }
 
-
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetAxisRaw("Vertical") == -1)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(-30f, 0, 0), step);
         }
+        else
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(30f, 0, 0), back_step);
+        }
 
-
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetAxisRaw("Horizontal") == -1)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, 30f), step);
         }
+        else
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, -30), back_step);
+        }
 
-
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetAxisRaw("Horizontal") == 1)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, -30f), step);
         }
-
-        else if(Input.GetKey(KeyCode.None))
+        else
         {
-            transform.localEulerAngles = Vector3.zero;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, 30f), back_step);
         }
 
     }
